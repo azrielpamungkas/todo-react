92bbb7f0b708c988552eb4735ec316a04eb25b3c
Azriel Pamungkas <github.rwcoy@simplelogin.co>
2023-12-25 21:08:38 +0700

    Add CSS to components

--- src/TodoItem.jsx
+++ src/TodoItem.jsx
@@ -1,5 +1,16 @@
 const TodoItem = ({ todo }) => {
-  return <p>{todo.title}</p>;
+  return (
+    <div style={styles.todoItem}>
+      <p>{todo.title}</p>
+    </div>
+  );
+};
+
+const styles = {
+  todoItem: {
+    border: "2px solid gray",
+    fontSize: "16px",
+  },
 };
 
 export default TodoItem;
