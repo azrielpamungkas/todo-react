120ecbb00098fdc2f8eaf98127affb08e8388196
Azriel Pamungkas <github.rwcoy@simplelogin.co>
2023-12-25 20:58:05 +0700

    create Todos and TodoItem component

---
+++ src/Todos.jsx
@@ -0,0 +1,12 @@
+import TodoItem from "./TodoItem";
+const Todos = ({ todos }) => {
+  return (
+    <>
+      {todos.map((todo) => {
+        return <TodoItem key={todo.id} todo={todo} />;
+      })}
+    </>
+  );
+};
+
+export default Todos;
